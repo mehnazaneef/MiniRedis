7a64d7ad79f779988b1f405425869361fdbdb88c
Mehnaz <mehnazaneef@gmail.com>
2026-07-15 23:53:27 +0530

    Add command result response type

---
+++ include/CommandResult.h
@@ -0,0 +1,33 @@
+#pragma once
+#pragma once
+
+#include <string>
+
+struct CommandResult
+{
+    bool success;
+    std::string message;
+
+    CommandResult(bool success,
+        std::string message)
+        : success(success),
+        message(std::move(message))
+    {
+    }
+
+    static CommandResult ok(std::string message)
+    {
+        return CommandResult(
+            true,
+            std::move(message)
+        );
+    }
+
+    static CommandResult error(std::string message)
+    {
+        return CommandResult(
+            false,
+            std::move(message)
+        );
+    }
+};
